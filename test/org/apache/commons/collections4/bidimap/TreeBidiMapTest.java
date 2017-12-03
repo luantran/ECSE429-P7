9df15919534bbbfc796a6a7c79f73fc7548bb80b
the-luantran <the-luan.tran@mail.mcgill.ca>
2017-12-03 16:52:05 -0500

    fixed another shit

--- test/org/apache/commons/collections4/bidimap/TreeBidiMapTest.java
+++ test/org/apache/commons/collections4/bidimap/TreeBidiMapTest.java
@@ -1,5 +1,6 @@
 package org.apache.commons.collections4.bidimap;
 
+import data.*;
 import static org.junit.Assert.*;
 
 import org.junit.After;
@@ -20,6 +21,7 @@ public class TreeBidiMapTest {
 
 	@Before
 	public void setUp() throws Exception {
+		TreeBidiMap newBidiMap = T
 	}
 
 	@After
@@ -150,5 +152,92 @@ public class TreeBidiMapTest {
 	public void testToString() {
 		fail("Not yet implemented");
 	}
+	
+	/*
+	 * Notes:
+	 * - For compare(a1,b1)
+	 * 	- a1 > b1 --> return a positive value
+	 * 	- a1 < b1 --> return a negative value
+	 * 	- a1 == b1 --> return 0
+	 */
+	
+	/*
+	 * Testing for method doPut; will test through method Put
+	 * 
+	 * Test Case 1 (line 509):
+	 * - Need node == null --> checking root
+	 * - Done with empty Map
+	 * - Verify --> get first node and confirm key and value pair
+	 * 
+	 * 
+	 */
+
+	/*
+     * Testing for method doPut; will test through method Put
+     *
+     * Test Case 2 (line 521):
+     * - Need cmp == 0 --> make key of key, value pair the same as the root
+     * - Done with Map with one value in root
+     * - Verify --> get an IllegalArgumentException
+     *
+     */
+	
+	/*
+	 * Testing for method doPut; will test through method Put
+	 * 
+	 * Test Case 3 (line 524 and 528):
+	 * - Need cmp < 0 --> key in root must be greater than the key in value
+	 * - Done with Map with one value in root
+	 * - Verify:
+	 * 	- the left child has the same key and value pair as the one passed in the put
+	 * 	- the right child is null
+	 * 
+	 */
+	
+	/*
+	 * Testing for method doPut; will test through method Put
+	 * 
+	 * Test Case 4 (line 538 and 542):
+	 * - Need cmp > 0 --> key in root must be less than the key in value
+	 * - Done with Map with one value in root
+	 * - Verify:
+	 * 	- the right child has the same key and value pair as the one passed in the put
+	 * 	- the left child is null
+	 * 
+	 */
+	
+	/*
+	 * Testing for method doPut; will test through method Put
+	 * 
+	 * Test Case 5 (line 524, 525 and 528):
+	 * - Need:
+	 * 	- cmp < 0 --> key in root must be greater than the key in value
+	 * 	- Key of the left node, new "node", needs to be greater than the key in value
+	 * - Done with Map with one value in root, and root with a left child
+	 * - Verify:
+	 * 	- verify that the right child is null
+	 *  - that there exist a left child
+	 *  	- put this in a new variable
+	 * 	- the new left child has the same key and value pair as the one passed in the put
+	 * 	- the new right child is null
+	 * 
+	 */
+	
+	/*
+	 * Testing for method doPut; will test through method Put
+	 * 
+	 * Test Case 6 (line 538, 539, and 542):
+	 * - Need:
+	 * 	- cmp > 0 --> key in root must be less than the key in value
+	 * 	- Key of the right node, new "node", needs to be less than the key in value
+	 * - Done with Map with one value in root, and root with a right child
+	 * - Verify:
+	 * 	- verify that the left child is null
+	 *  - that there exist a right child
+	 *  	- put this in a new variable
+	 * 	- the new right child has the same key and value pair as the one passed in the put
+	 * 	- the new left child is null
+	 * 
+	 */
 
 }
