cba34c1e23682dad56a1cd2f5ef63e8886cf5f26
the-luantran <the-luan.tran@mail.mcgill.ca>
2017-12-03 16:50:23 -0500

    fixed mssing test folder

---
+++ test/org/apache/commons/collections4/bidimap/TreeBidiMapTest.java
@@ -0,0 +1,154 @@
+package org.apache.commons.collections4.bidimap;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TreeBidiMapTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTreeBidiMap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTreeBidiMapMapOfQextendsKQextendsV() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContainsKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContainsValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPutAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFirstKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLastKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNextKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPreviousKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKeySet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValues() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEntrySet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMapIterator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInverseBidiMap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
