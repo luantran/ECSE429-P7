d1b13aa8ee88ef8b0517ccfaa4a7faea7dd26884
ahtml <anadhtml@gmail.com>
2017-12-03 16:14:47 -0500

    Added some test data in TreeBidiMapData.

--- src/data/TreeBidiMapData.java
+++ src/data/TreeBidiMapData.java
@@ -4,5 +4,39 @@ import org.apache.commons.collections4.BidiMap;
 import org.apache.commons.collections4.bidimap.TreeBidiMap;
 
 public class TreeBidiMapData {
+	
+	public BidiMap emptyTreeBidiMap(){
+		
+		BidiMap emptyMap = new TreeBidiMap();
+		return emptyMap;
+		
+	}
+	
+	public BidiMap withRootBidiMap() {
+		
+		BidiMap mapRoot = new TreeBidiMap<>();
+		mapRoot.put(5, 10);
+		return mapRoot;
+		
+	}
+	
+	public BidiMap valueOnLeftChild() {
+		
+		BidiMap leftChildMap = new TreeBidiMap();
+		leftChildMap.put(5, 10);
+		leftChildMap.put(3, 27);
+		return leftChildMap;
+		
+	}
+	
+	public BidiMap valueOnRightChild() {
+		
+		BidiMap rightChildMap = new TreeBidiMap();
+		rightChildMap.put(5, 10);
+		rightChildMap.put(7, 12);
+		return rightChildMap;
+	}
+	
+	
 
 }
