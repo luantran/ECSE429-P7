a38fb81d6414c8b1491bad86130b72d6c8ad723c
Shamil P. Gretzky <shamilgretzky@gmail.com>
2017-12-04 13:50:56 -0500

    Adding CheckKey/CheckValue Tests

--- test/org/apache/commons/collections4/bidimap/BlackBoxTests.java
+++ test/org/apache/commons/collections4/bidimap/BlackBoxTests.java
@@ -32,29 +32,102 @@ public class BlackBoxTests {
 	}
 
 	/*
-	 * No errors, returns true
+	 * No errors, returns true for lookupKeyTest
 	 */
 	@Test
-	public void lookupTest1() {
-		assertTrue(bigBidiMap.containsKey(5));
-		
+	public void lookupKeyTest1() {
+		assertTrue(bigBidiMap.containsKey(5));	
 	}
 	
 	/*
-	 * No errors, returns false
+	 * No errors, returns false for lookupKeyTest
 	 */
 	@Test
-	public void lookupTest2() {
+	public void lookupKeyTest2() {
 		assertFalse(bigBidiMap.containsKey(8));
-		
 	}
 	
 	/*
-	 * 
+	 * No errors, returns null for lookupKeyTest
 	 */
 	@Test(expected=NullPointerException.class)
-	public void lookupTest3() {
+	public void lookupKeyTest3() {
 		bigBidiMap.containsKey(null);
-		
 	}
+	
+	/*
+	 * No errors, returns true for lookupValueTest
+	 */
+	@Test
+	public void lookupValueTest1() {
+		assertTrue(bigBidiMap.containsValue(10));
+	}
+	
+	/*
+	 * No errors, returns false for lookupValueTest
+	 */
+	@Test
+	public void lookupValueTest2() {
+		assertFalse(bigBidiMap.containsValue(11));
+	}
+	
+	/*
+	 * No errors, returns null for lookupKeyTest
+	 */
+	@Test(expected=NullPointerException.class)
+	public void lookupValueTest3() {
+		bigBidiMap.containsValue(null);
+	}
+	
+	
+	/*
+	 * The Following "checkKeyTest" Relate to lookupKey
+	 * No errors, returns true for checkKeyTest
+	 */
+	@Test
+	public void checkKeyTest1() {
+		assertTrue(bigBidiMap.containsKey(5));	
+	}
+	
+	/*
+	 * No errors, returns false for checkKeyTest
+	 */
+	@Test
+	public void checkKeyTest2() {
+		assertFalse(bigBidiMap.containsKey(8));
+	}
+	
+	/*
+	 * No errors, returns null for checkKeyTest
+	 */
+	@Test(expected=NullPointerException.class)
+	public void checkKeyTest3() {
+		bigBidiMap.containsKey(null);
+	}
+	
+	/*
+	 * The Following "checkValueTest" Relate to lookupValue
+	 * No errors, returns true for checkValueTest
+	 */
+	@Test
+	public void checkValueTest1() {
+		assertTrue(bigBidiMap.containsValue(10));
+	}
+	
+	/*
+	 * No errors, returns false for checkValueTest
+	 */
+	@Test
+	public void checkValueTest2() {
+		assertFalse(bigBidiMap.containsValue(11));
+	}
+	
+	/*
+	 * No errors, returns null for checkValueTest
+	 */
+	@Test(expected=NullPointerException.class)
+	public void checkValueTest3() {
+		bigBidiMap.containsValue(null);
+	}
+	
 }
