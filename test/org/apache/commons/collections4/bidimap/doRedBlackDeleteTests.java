3c495e852912b5d1e8faaf331de1d02371b14bc5
ahtml <anadhtml@gmail.com>
2017-12-04 19:34:40 -0500

    Added asserts to make sure that the key was no longer in the Tree.

--- test/org/apache/commons/collections4/bidimap/doRedBlackDeleteTests.java
+++ test/org/apache/commons/collections4/bidimap/doRedBlackDeleteTests.java
@@ -34,7 +34,7 @@ public class doRedBlackDeleteTests {
 		rootOnlyMap = dataClass.withRootOnlyBidiMap();
 		emptyMap = dataClass.emptyTreeBidiMap();
 		leftChildMap = dataClass.valueOnLeftChild();
-		// bigBidiMap = dataClass.bigBidiMap();
+		bigBidiMap = dataClass.bigBidiMap();
 		rightChildMap = dataClass.valueOnRightChild();
 		twoLevelTree = dataClass.twoLevelBidiMap();
 		
@@ -51,9 +51,9 @@ public class doRedBlackDeleteTests {
 	 */
 	@Test
 	public void testRemoveRoot() {
-//		rootOnlyMap.remove(5);
-//		assertEquals(0,rootOnlyMap.size());
-		
+		rootOnlyMap.remove(5);
+		assertEquals(0,rootOnlyMap.size());
+		assertEquals(false, rootOnlyMap.containsKey(5)); // Make sure the key is no longer there
 	}
 	
 	/*
@@ -64,8 +64,9 @@ public class doRedBlackDeleteTests {
 	 */
 	@Test
 	public void testRemoveRoot2() {
-//		twoLevelTree.remove(20);
-//		assertEquals(2,twoLevelTree.size());
+		twoLevelTree.remove(20);
+		assertEquals(2,twoLevelTree.size());
+		assertEquals(false, twoLevelTree.containsKey(20)); // Make sure the key is no longer there
 	}
 	
 	/*
@@ -78,6 +79,31 @@ public class doRedBlackDeleteTests {
 	public void testRemoveLeftChild() {
 		twoLevelTree.remove(10);
 		assertEquals(2,twoLevelTree.size());
+		assertEquals(false, twoLevelTree.containsKey(10)); // Make sure the key is no longer there
+	}
+	
+	/*
+	 * Deleting right child when the tree is balanced and height of 1
+	 * Coverage:
+	 * 1014
+	 */
+	@Test
+	public void testRemoveRightChild() {
+		twoLevelTree.remove(30);
+		assertEquals(2,twoLevelTree.size());
+		assertEquals(false, twoLevelTree.containsKey(30)); // Make sure the key is no longer there
+	}
+	
+	/*
+	 * Deleting child which is both black and on the right side
+	 * Coverage:
+	 * 972, 986, 994
+	 * 1012
+	 */
+	@Test
+	public void testRemoveOnRight() {
+		 bigBidiMap.remove(12);
+		 assertEquals(false, bigBidiMap.containsKey(12)); // Make sure the key is no longer there
 	}
 
 }
